db9eaae75c07bba0a52d92c666f98ff229777f77
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-09-06 13:30:29 +0200

    - Added initial files - Proving to group that git works :)

---
+++ src/obligEn/Main.java
@@ -0,0 +1,12 @@
+package obligEn;
+
+public class Main{
+    public static void main(String []args){
+        Oblig1 ob1 = new Oblig1();
+
+        int[] a = {1,2,3,4,5};
+        System.out.println(ob1.antallUlikeSortert(a));
+        System.out.println("Antall ombyttinger er "+ ob1.ombyttinger(a));
+        System.out.println("Verdien til det største tallet i listen er "+ob1.maks(a));
+        }
+}
