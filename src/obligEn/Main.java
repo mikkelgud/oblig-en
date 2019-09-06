37cc03f10555fbc20b1f1dd3b42d2a17198b4c17
jorgen <jmobakken@gmail.com>
2019-09-06 15:22:33 +0200

    har lagt til litt i oppgave 3, men må få sortert arrayet riktig for å få rett resultat, tror dermed at oppgave 1 er litt feil

--- src/obligEn/Main.java
+++ src/obligEn/Main.java
@@ -3,9 +3,10 @@ package obligEn;
 public class Main{
     public static void main(String []args){
 
-        int[] a = {1,3,5,6,7};
-        System.out.println("Antall ulike sorter er "+Oppg2.antallUlikeSortert(a));
+        int[] a = {1,7,6,3,3,4};
+        System.out.println("Verdien til det største tallet i listen er "+Oppg1.maks(a)); //er listen egt sortert?
         System.out.println("Antall ombyttinger er "+ Oppg1.ombyttinger(a));
-        System.out.println("Verdien til det største tallet i listen er "+Oppg1.maks(a));
+        System.out.println("Antall ulike sorter er "+Oppg2.antallUlikeSortert(a));
+        System.out.println("Antall ulike tall er "+Oppg3.antallUlikeSortert(a)); //feil må få sortert liste først
         }
 }
--- src/obligEn/Oppg3.java
+++ src/obligEn/Oppg3.java
@@ -1,4 +1,5 @@
 package obligEn;
+//får ikke ut riktig siden man må sortere listen for å få ut riktig tall. nå sjekker den kunn tall ved siden av hverandre
 
 public class Oppg3 {
 
@@ -7,13 +8,13 @@ public class Oppg3 {
         int teller = 1;
 
         if (a.length == 0){
-            teller = 0;
+            return 0;
         }
 
-        for (int i = 0; i < a.length; i++){
-
-
-
+        for (int i = 1; i < a.length; i++){
+            if(a[i] == a[i-1]){
+                teller++;
+            }
         }
 
         return teller;
