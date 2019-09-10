7867dc2e6080c9f34e03bcf0b0bcacb96ee4f825
jorgen <jmobakken@gmail.com>
2019-09-10 13:11:36 +0200

    Merge branch 'master' of C:\Users\joerg\Documents\algdatobliger\oblig-en with conflicts.

--- src/obligEn/Oppg5.java
+++ src/obligEn/Oppg5.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class Oppg5 {
 
-    public static void rotasjon(char[] a){
+    public static void rotasjon(int[] a){
         int d = 1;
         int n = a.length;  if (n < 2) return;
         if ((d %= n) < 0) d += n;
 
-        char[] b = Arrays.copyOfRange(a, n - d, n);
+        int[] b = Arrays.copyOfRange(a, n - d, n);
         for (int i = n - 1; i >= d; i--) a[i] = a[i - d];
         System.arraycopy(b, 0, a, 0, d);
 
