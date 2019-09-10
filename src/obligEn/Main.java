899caf2edd7fed13d2b24c1460d89d04c866009e
felix <felixfotball99@gmail.com>
2019-09-10 11:32:52 +0200

    - Oppg4 done, tror jeg. --> Brukte while istede for, for-løkke. --> Skal funke for alle heltall.

--- src/obligEn/Main.java
+++ src/obligEn/Main.java
@@ -3,11 +3,12 @@ package obligEn;
 public class Main{
     public static void main(String []args){
 
-        int[] a = {1,3,5,3,10,2};
+        int[] a = {1,3,5,3,10,2,8};
         System.out.println("Antall ombyttinger er "+ Oppg1.ombyttinger(a));
         System.out.println("Verdien til det største tallet i listen er "+Oppg1.maks(a));
-        System.out.println("Antall ulike sorter er "+Oppg2.antallUlikeSortert(a));
+       // System.out.println("Antall ulike sorter er "+Oppg2.antallUlikeSortert(a));
         System.out.println("Antall ulike sorter er "+Oppg3.antallUlikeSortert(a));
+        Oppg4.delsortering(a);
 
 
 
--- src/obligEn/Oppg3.java
+++ src/obligEn/Oppg3.java
@@ -11,7 +11,7 @@ public class Oppg3 {
         }
 
         for (int i = 0; i < a.length; i++){
-            boolean unik = false;
+            boolean unik     = false;
 
             for (int j = 0; j<i; j++){
                 if (a[j] == a[i]){
