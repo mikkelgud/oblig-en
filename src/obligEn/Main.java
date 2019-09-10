fa7d8886169391f6f1f0333581f6844b9f0817c3
jorgen <jmobakken@gmail.com>
2019-09-10 11:25:55 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/oblig-en/.idea/workspace.xml
    #	out/production/oblig-en/obligEn/Main.class
    #	src/obligEn/Main.java
    
    oppgave 3 funker nå

--- src/obligEn/Main.java
+++ src/obligEn/Main.java
@@ -3,9 +3,9 @@ package obligEn;
 public class Main{
     public static void main(String []args){
 
-        int[] a = {1,5,3,4,5,5,7,8,9,10};
+        int[] a = {1,7,6,3,3,4,7,7,7};
+        System.out.println("Verdien til det største tallet i listen er "+Oppg1.maks(a)); //er listen egt sortert?
         System.out.println("Antall ombyttinger er "+ Oppg1.ombyttinger(a));
-        System.out.println("Verdien til det største tallet i listen er "+Oppg1.maks(a));
         System.out.println("Antall ulike sorter er "+Oppg2.antallUlikeSortert(a));
         System.out.println("Antall ulike tall er "+Oppg3.antallUlikeSortert(a)); //feil må få sortert liste først
         }
--- src/obligEn/Oppg3.java
+++ src/obligEn/Oppg3.java
@@ -4,7 +4,7 @@ public class Oppg3 {
 
     public static int antallUlikeSortert(int[] a){
 
-        int teller = 1;
+        int teller = 0;
 
         if (a.length == 0){
             teller = 0;
@@ -13,7 +13,7 @@ public class Oppg3 {
         for (int i = 0; i < a.length; i++){
             boolean unik = false;
 
-            for (int j = 0; j<i; j++){
+            for (int j = 0; j < i; j++){
                 if (a[j] == a[i]){
                     unik = true;
                     break;
