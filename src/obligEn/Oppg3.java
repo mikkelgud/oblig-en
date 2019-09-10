b3dec4fec025358efd3cb1a51e16b1bd91b8090d
felix <felixfotball99@gmail.com>
2019-09-10 10:52:14 +0200

    - Oppg3 done. --> Antall uniketall når listen er usortert.

--- src/obligEn/Oppg3.java
+++ src/obligEn/Oppg3.java
@@ -4,19 +4,25 @@ public class Oppg3 {
 
     public static int antallUlikeSortert(int[] a){
 
-        int teller = 1;
+        int teller = 0;
 
         if (a.length == 0){
             teller = 0;
         }
 
         for (int i = 0; i < a.length; i++){
+            boolean unik = false;
 
-
-
+            for (int j = 0; j<i; j++){
+                if (a[j] == a[i]){
+                    unik = true;
+                    break;
+                }
+            }
+            if (!unik){
+                teller++;
+            }
         }
-
         return teller;
     }
-
 }
