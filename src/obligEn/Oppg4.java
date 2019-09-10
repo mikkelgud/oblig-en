c463908bb7c9e5afee6808d4c4f4b474963b0dd7
felix <felixfotball99@gmail.com>
2019-09-10 12:18:21 +0200

    - Oppg4 fix.

--- src/obligEn/Oppg4.java
+++ src/obligEn/Oppg4.java
@@ -5,28 +5,24 @@ import java.util.Arrays;
 public class Oppg4{
 
    public static void delsortering(int []a) {
-       int venstre = 0;
-       int høyre = a.length - 1;
 
        int oddeTallCount = 0;
 
-       while (venstre < høyre) {
-           while (a[venstre] % 2 != 0) {
-               venstre++;
-               oddeTallCount++;
+       for (int i = 0; i < a.length; i++)
+         if ((a[i] & 1) != 0){
+           a[i] *= -1;
+           oddeTallCount++;
            }
-           while (a[høyre] % 2 == 0 && venstre < høyre) {
-               høyre--;
 
-               if (venstre < høyre) {
-                   int temp = a[venstre];
-                   a[venstre] = a[høyre];
-                   a[høyre] = temp;
-               }
+       Arrays.sort(a);
+
+       for (int i = 0; i < a.length; i++) {
+           if ((a[i] & 1) != 0) {
+               a[i] *= -1;
            }
-           Arrays.sort(a, 0, oddeTallCount);
-           Arrays.sort(a, oddeTallCount, a.length);
        }
+       Arrays.sort(a, 0, oddeTallCount);
        System.out.println(Arrays.toString(a));
+
    }
 }
