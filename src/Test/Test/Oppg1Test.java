f59193741fa9b19a25c14dc3493099a2113a59ec
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-09-11 20:38:59 +0200

    -Made testing for task 1

--- src/Test/Test/Oppg1Test.java
+++ src/Test/Test/Oppg1Test.java
@@ -2,24 +2,36 @@ package Test;
 
 import obligEn.Oppg1;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Oppg1Test {
 
     int testArray[] ={23,2,3,24,23,2,4453,645,56,765,76};
+    int testArrayNegative[] ={-12,-3,-13,-16};
 
     @Test
-    public void ReturnererMaksStørsteVerdi() {
-        Oppg1.maks(testArray);
+    public void ReturnererMaksStørsteVerdiFraEtPositivtArray() {
+        int expected = 4453;
+        int actualHigestNumberFromMethod = Oppg1.maks(testArray);
 
+        Assertions.assertEquals( actualHigestNumberFromMethod, expected);
+    }
+    @Test
+    public void ReturnererMaksStørsteVerdiFraEtNegativeArray() {
+        int expected = -3;
+        int actualHigestNumberFromMethod = Oppg1.maks(testArrayNegative);
 
-
-
+        Assertions.assertEquals( actualHigestNumberFromMethod, expected);
     }
 
     @Test
     public void fårFunksjonRiktigOmbyttinger() {
+        int expectedPermutations = 8;
+        int actualPermutations = Oppg1.ombyttinger(testArray);
+
+        Assertions.assertEquals( actualPermutations, expectedPermutations);
 
     }
 }
