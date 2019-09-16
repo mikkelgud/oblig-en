e9d1b4bd6ec0de9da3a023b8803e28515e2fb06c
felix <felixfotball99@gmail.com>
2019-09-16 13:40:09 +0200

    mista opp8 så pusher igjen

--- src/obligEn/Oppg8.java
+++ src/obligEn/Oppg8.java
@@ -1,15 +1,45 @@
 package obligEn;
 
+import java.util.Arrays;
+
 public class Oppg8 {
-    public static int[] indekssortering(int[] a){
+
+    public static int[] indekssortering(int[] a) {
+
         int[] indeks = new int[a.length];
 
+        //Setter en verdi lik max av det en int kan være.
+        final int MAX_VALUE_OF_AN_INTEGER = Integer.MAX_VALUE;
 
-
-        System.out.println(Arrays.toString(indeks));
-
-
+        //Iterer gjennom for hvert ledd av arrayet.
+        for (int i = 0; i < a.length; i++){
+            //Finner indeksen til minste verdi
+            int currentMinIndex = findMin(a);
+            //Setter indeks sitt i´te ledd til iterasjonens index hentet fra findMin() metoden.
+            indeks[i] = currentMinIndex;
+            //Skifter ut det nåverende laveste leddet med et teit høyt tall.
+            a[currentMinIndex] = MAX_VALUE_OF_AN_INTEGER;
+        }
         return indeks;
     }
 
+
+    public static int findMin(int[] a){
+        //Bruker min som hjelpevariabel for å holde på verdien til current index.
+        int min = a[0];
+        //Instansierer 1. index til 0.
+        int currentMinIndex = 0;
+
+        //Itererer gjennom alle tall i listen.
+        for (int i = 0; i < a.length; i++){
+            // Sjekker om arrayet til i'te posisjon er mindre enn det nåværende minste tallet. Og setter dette til det nye minste tallet og tar vare på indeksen.
+            if (a[i] < min){
+                currentMinIndex = i;
+                min = a[i];
+            }
+        }
+        return currentMinIndex;
+    }
+
 }
+
