f52a717cc49cfb21a7643e44b14ed364eba5b573
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-09-18 14:34:43 +0200

    - Task 10 is now working as it should I suppose. - Still need to make it more sufficient

--- src/obligEn/Main.java
+++ src/obligEn/Main.java
@@ -6,7 +6,7 @@ public class Main{
     public static void main(String []args){
 
         int[] a = {2, 3, 4, 1, 5, 6};
+        System.out.println(Oblig1.inneholdt("ABBA", "BARBERER"));
 
-        System.out.println(Arrays.toString(Oblig1.tredjeMin(a)));
         }
 }
